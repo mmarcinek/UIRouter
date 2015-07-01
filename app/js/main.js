bd0c8338032ff8fc01eda3cee7e0ed4b69b52299
Michael Marcinek <michaelmarcinek@gmail.com>
2015-06-30 23:22:28 -0400

    Saves

--- app/js/main.js
+++ app/js/main.js
@@ -1,58 +1,61 @@
-;(function (){
-
+(function() {
   'use strict';
 
   angular.module('Pretzels', ['ui.router'])
 
   .config(['$stateProvider', '$urlRouterProvider',
+      function ($stateProvider, $urlRouterProvider) {
 
-    function ($stateProvider, $urlRouterProvider){
+        $urlRouterProvider.otherwise('/');
 
-      $urlRouterProvider.otherwise('/');
-
-      $stateProvider
-
-        .state('home', {
-          url: '/',
-          views: {
-            'header': {
-              templateUrl: 'js/templates/header.tpl.html'
-            },
-            'footer': {
-              templateUrl: 'js/templates/footer.tpl.html'
-            },
-            'sidebar' : {
-              templateUrl: 'js/templates/sidebar.tpl.html',
-              controller: 'PretzelCtrl'
-            },
-            'content': {
-              templateUrl: 'js/templates/home.tpl.html'
+        $stateProvider
+          .state('home', {
+            url: '/',
+            views: {
+              'header': {
+                templateUrl: 'js/templates/header.tpl.html'
+              },
+              'footer': {
+                templateUrl: 'js/templates/footer.tpl.html'
+              },
+              'content': {
+                templateUrl: 'js/templates/home.tpl.html'
+              },
+              'sidebar': {
+                templateUrl: 'js/templates/sidebar.tpl.html',
+                controller: 'PretzelCtrl'
+              }
             }
-          }
-        })
-        .state('about', {
-          url: '/about',
-          templateUrl: 'js/templates/about.tpl.html'
-        });
+          });
+      }
+  ])
 
+  .controller('PretzelCtrl', ['$scope',
+    function ($scope) {
+      $scope.pretzels = [
+        {
+          id: 1,
+          brand: 'Philly',
+          type: 'Soft'
+        },
+        {
+          id: 2,
+          brand: 'Rold Gold',
+          type: 'Twist'
+        },
+        {
+          id: 3,
+          brand: 'Rold Gold',
+          type: 'Squares'
+        },
+        {
+          id: 4,
+          brand: 'Snyders',
+          type: 'Honey Mustard'
+        }
+      ];
     }
+  ]);
 
-    ])
-
-   .controller('PretzelCtrl', ['$scope',
-          function ($scope){
-            $scope.pretzels = [
-            {
-              id: 1,
-              type: 'Soft'
-            },
-            {
-              id: 2,
-              type: 'Bavarian'
-            }
-
-            ];
-          }
-      ]);
 
 }());
